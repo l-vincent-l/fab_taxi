f5e540526bdd56c82d45e201f43b8361def4c2b5
l-vincent-l <lara.vincent@gmail.com>
2016-02-24 17:52:47 +0100

    Set parent_id on testing zupc

--- files/add_departement_zupc.sql
+++ files/add_departement_zupc.sql
@@ -10,11 +10,14 @@ $$;
 DO LANGUAGE plpgsql $$
     DECLARE
     departement_id int;
+    zupc_id int;
     BEGIN
         SELECT id INTO departement_id FROM departement WHERE nom='testing';
         IF (SELECT COUNT(id)::int FROM "ZUPC" WHERE nom = 'testing') = 0 THEN 
             INSERT INTO "ZUPC" ("departement_id", "nom", "insee", "shape", active) VALUES (departement_id, 'testing', '999999', ST_geomFROMGEOJSON('{"type": "MultiPolygon","coordinates": [[[[0.0, 0.0], [0.0, 1.0], [1.0, 1.0],[1.0, 0.0], [0.0, 0.0] ]]]}'), true);
             RAISE NOTICE 'zupc testing has been inserted';
         END IF;
+        SELECT id INTO zupc_id FROM "ZUPC" where nom = 'testing';
+        UPDATE "ZUPC" SET parent_id = zupc_id where id = zupc_id;
     END;
 $$;
