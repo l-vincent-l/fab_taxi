677b820b3479288d01e9d445c363f4a92abafeb4
l-vincent-l <lara.vincent@gmail.com>
2016-03-07 18:05:09 +0100

    Use admin apikey

--- fabfile/test_api.py
+++ fabfile/test_api.py
@@ -3,6 +3,7 @@ from fabric.api import task, put, env, run, sudo
 from fabric.contrib import files
 import tempfile, json
 from jsondiff import diff
+from .api import get_admin_key
 
 def get_diff(expected, result):
     return diff(json.loads(expected), json.loads(result))
@@ -30,42 +31,38 @@ def except_id_added(expected, result):
 def no_diff(expected, result):
     return get_diff(expected, result) == {}
 
-def test_add_vehicle(curl):
+def test_add_vehicle(curl, apikey):
     local_file = 'files/vehicle.json'
     remote_file = '/tmp/testing_uwsgi_vehicle'
     put(local_file, remote_file)
-    header, data = curl('/vehicles/', env.conf_api.TESTING_APIKEY_OPERATEUR,
-            'POST', '@{}'.format(remote_file))
+    header, data = curl('/vehicles/', apikey, 'POST', '@{}'.format(remote_file))
     except_id_added(open(local_file).read(), data)
     return json.loads(data)['data'][0]['id']
 
 
-def test_add_ads(curl, vehicle_id):
+def test_add_ads(curl, apikey, vehicle_id):
     remote_file = '/tmp/ads.json'
     context = {'vehicle_id': vehicle_id}
     files.upload_template('templates/ads.json', remote_file, context=context)
-    header, data = curl('/ads/',  env.conf_api.TESTING_APIKEY_OPERATEUR, 'POST',
-            '@{}'.format(remote_file))
+    header, data = curl('/ads/',  apikey, 'POST', '@{}'.format(remote_file))
     expected = open('templates/ads.json').read() % context
     if not no_diff(expected, data):
         raise ValueError('{} {} has to be equal'.format(expected, data))
 
 
-def test_add_driver(curl):
+def test_add_driver(curl, apikey):
     remote_file = '/tmp/driver.json'
     put('files/driver.json', remote_file)
-    header, data = curl('/drivers/',  env.conf_api.TESTING_APIKEY_OPERATEUR,
-        'POST', '@{}'.format(remote_file))
+    header, data = curl('/drivers/',  apikey, 'POST', '@{}'.format(remote_file))
     expected = open('files/driver.json').read()
     if not no_diff(expected, data):
         raise ValueError('{} {} has to be equal diff: {}'.format(
                 expected, data, get_diff(expected, data)))
 
-def test_add_taxi(curl):
+def test_add_taxi(curl, apikey):
     remote_file = '/tmp/taxi.json'
     put('files/taxi.json', remote_file)
-    header, data = curl('/taxis/', env.conf_api.TESTING_APIKEY_OPERATEUR,
-        'POST', '@{}'.format(remote_file))
+    header, data = curl('/taxis/', apikey, 'POST', '@{}'.format(remote_file))
     expected = open('files/taxi_expected.json').read()
     except_id_added(expected, data)
     return json.loads(data)['data'][0]['id']
@@ -73,16 +70,12 @@ def test_add_taxi(curl):
 def send_position_taxi(taxi_id):
     remote_file = '/tmp/send_position_taxi.py'
     put('files/send_position_taxi.py', remote_file)
-    operator = run('psql -c "SELECT email FROM \\""user\\"" WHERE apikey=\'{}\';" {}'\
-            .format(env.conf_api.TESTING_APIKEY_OPERATEUR,
-        env.conf_api.SQLALCHEMY_DATABASE_URI)).split('\n')[2].strip()
     run('python {} {} {} {} {}'.format(remote_file, taxi_id,
-        env.conf_api.TESTING_APIKEY_OPERATEUR, env.geoserver_port, operator))
+        env.conf_api.TESTING_APIKEY_OPERATEUR, env.geoserver_port, 'admin'))
 
 
-def test_get_taxi(curl, taxi_id):
-    header, data = curl('/taxis/{}/'.format(taxi_id),
-            env.conf_api.TESTING_APIKEY_OPERATEUR)
+def test_get_taxi(curl, apikey, taxi_id):
+    header, data = curl('/taxis/{}/'.format(taxi_id), apikey)
     expected = open('files/taxi_expected.json').read()
     diff = get_diff(expected, data)
     check_root(diff)
@@ -92,9 +85,8 @@ def test_get_taxi(curl, taxi_id):
     assert json.loads(data)['data'][0]['id'] == taxi_id
 
 
-def test_get_taxis(curl, taxi_id):
-    header, data = curl('/taxis/?lon=0.1&lat=0.1'.format(taxi_id),
-            env.conf_api.TESTING_APIKEY_MOTEUR)
+def test_get_taxis(curl, apikey, taxi_id):
+    header, data = curl('/taxis/?lon=0.1&lat=0.1'.format(taxi_id), apikey)
     expected = open('files/taxi_expected.json').read()
     diff = get_diff(expected, data)
     check_root(diff)
@@ -124,11 +116,12 @@ def test_api(testing_file, socket, server_name):
         command = 'python {} {} "{}" {}'.format(testing_file, socket, 
                 server_name+v[0], " ".join(v[1:]))
         return run(command).split(u'\r\r\n\r\r\n', 1)
-    vehicle_id = test_add_vehicle(curl)
-    test_add_ads(curl, vehicle_id)
-    test_add_driver(curl)
-    taxi_id = test_add_taxi(curl)
+    apikey = get_admin_key().splitlines()[0].strip()
+    vehicle_id = test_add_vehicle(curl, apikey)
+    test_add_ads(curl, apikey, vehicle_id)
+    test_add_driver(curl, apikey)
+    taxi_id = test_add_taxi(curl, apikey)
     send_position_taxi(taxi_id)
-    test_get_taxi(curl, taxi_id)
-    test_get_taxis(curl, taxi_id)
+    test_get_taxi(curl, apikey, taxi_id)
+    test_get_taxis(curl, apikey, taxi_id)
     delete_testing_objects(taxi_id)
